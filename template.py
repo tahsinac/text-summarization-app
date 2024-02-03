7050affdc1c34bd1d87c52dbc755c6cedfd57cb8
Tahsin Ashraf <tahsin.ac@live.com>
2024-02-03 11:48:06 -0500

    created file for programatically create directories

---
+++ template.py
@@ -0,0 +1,59 @@
+"""
+Code from file template.py is used to create directories and files based on a predefined list of file paths. It also checks if a file already exists and creates an empty file if it doesn't.
+
+- The code imports necessary modules and sets up logging.
+- It defines the project name and a list of file paths.
+- It iterates over each file path and creates the corresponding directory if it doesn't exist.
+- If the file doesn't exist or its size is 0, it creates an empty file.
+- Otherwise, it logs that the file already exists.
+"""
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+project_name = "textSummarizer"
+
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/common.py",
+    f"src/{project_name}/logging/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "params.yaml",
+    "app.py",
+    "main.py",
+    "Dockerfile",
+    "requirements.txt",
+    "setup.py",
+    "research/trials.ipynb",
+]
+
+# The `for filepath in list_of_files:` loop iterates over each file path in the `list_of_files` list.
+# It allows the code to perform operations on each file path individually, such as creating
+# directories and files based on the file path.
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory:{filedir} for the file {filename}")
+
+    
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath,'w') as f:
+            pass
+            logging.info(f"Creating empty file: {filepath}")
+
+    
+    else:
+        logging.info(f"{filename} already exists")
