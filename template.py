659b84e5a3289c001dadaabe6a57bfebac001d44
Tahsin Ashraf <tahsin.ac@live.com>
2024-02-03 12:32:00 -0500

    created dir structure using template.py

--- template.py
+++ template.py
@@ -54,6 +54,5 @@ for filepath in list_of_files:
             pass
             logging.info(f"Creating empty file: {filepath}")
 
-    
     else:
         logging.info(f"{filename} already exists")
